7c3131aca88d18fb2975f36934e2d40f07173c1d
Sachin Godishela <sachingodishela@outlook.com>
2021-05-15 11:58:35 +0530

    Initialized 4th project

---
+++ 4 Using External Shader Files/Main.cpp
@@ -0,0 +1,166 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <math.h>
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+double size = 0;
+
+
+// NEW CODE BLOCK 1
+/////////////////////////////////////////////////////////////////////////////
+																		  
+void printShaderLog(GLuint shader) {									  
+	int len = 0;														  
+	int chWrittn = 0;													  
+	char *log;															  
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);					  
+	if (len > 0) {														  
+		log = (char*)malloc(len);										  
+		glGetShaderInfoLog(shader, len, &chWrittn, log);				  
+		std::cout << "Shader info log: " << log << std::endl;			  
+		free(log);														  
+	}																	  
+	return;																  
+}																		  
+																		  
+void printProgramLog(GLuint program) {									  
+	int len = 0;														  
+	int chWrittn = 0;													  
+	char* log;															  
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);					  
+	if (len > 0) {														  
+		log = (char*)malloc(len);										  
+		glGetProgramInfoLog(program, len, &chWrittn, log);				  
+		std::cout << "Program info log: " << log << std::endl;			  
+		free(log);														  
+	}																	  
+	return;																  
+}																		  
+																		  
+bool checkOpenGLError() {												  
+	bool foundError = false;											  
+	int glErr = glGetError();											  
+	while (glErr != GL_NO_ERROR) {										  
+		std::cout<< "glError: " << glErr << std::endl;					  
+		foundError = true;												  
+		glErr = glGetError();											  
+	}																	  
+	return foundError;													  
+}																		  
+
+// END OF NEW CODE BLOCK 1
+/////////////////////////////////////////////////////////////////////////////
+
+GLuint createShaderProgram(GLFWwindow* window) {
+	const char* vShaderSource =
+		"#version 430 \n"
+		"void main(void) \n"
+		"{gl_Position = vec4(0,0,0,1);}";
+
+	const char* fShaderSource =
+		"#version 430 \n"
+		"out vec4 color;"
+		"void main(void) { \n"
+		"color = vec4(1.0, 1.0, 1.0, 0.0);"
+		"}";
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vShaderSource, NULL);
+	glShaderSource(fShader, 1, &fShaderSource, NULL);
+
+
+	glCompileShader(vShader);
+
+	// NEW CODE BLOCK 2
+	//////////////////////////////////////////////////////////////////////////////
+	checkOpenGLError();														   
+	GLint vertCompiled;														   
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);				   
+	if (vertCompiled != 1) {												   
+		std::cout << "Vertex shader compilation failed.." << std::endl;		   
+		printShaderLog(vShader);											   
+	}	
+	// END OF NEW CODE BLOCK 2
+	//////////////////////////////////////////////////////////////////////////////
+
+	glCompileShader(fShader);
+
+	// NEW CODE BLOCK 3
+	//////////////////////////////////////////////////////////////////////////////
+	checkOpenGLError();														   
+	GLint fragCompiled;														   
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);				   
+	if (fragCompiled != 1) {												   
+		std::cout << "Fragment shader compilation failed.. " << std::endl;	   
+		printShaderLog(fShader);											   
+	}				
+	// END OF NEW CODE BLOCK 3
+	//////////////////////////////////////////////////////////////////////////////
+
+	GLint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+
+	// NEW CODE BLOCK 4
+	///////////////////////////////////////////////////////////////////////////////
+	checkOpenGLError();															
+	GLint linked;																
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);							
+	if (linked != 1) {															
+		std::cout << "Linking Failed.." << std::endl;							
+		printProgramLog(vfProgram);												
+	}					
+	// END OF NEW CODE BLOCK 4
+	///////////////////////////////////////////////////////////////////////////////
+	return vfProgram;
+}
+
+//  initialize the shader program
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram(window);
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+// display loop
+void display(GLFWwindow* window) {
+	float r = 0.08, g = r * 2, b = g * 1.5;
+	glClearColor(r, g, b, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(renderingProgram);
+	bool flag;
+	size++;
+	double x = abs(255/** sin((3*size/100.0))*/ * cos((5 * size / 100.0)));
+	GLint pointsize;
+	glGetIntegerv(GL_POINT_SIZE, &pointsize);
+	//std::cout << pointsize << std::endl;
+	glEnable(GL_PROGRAM_POINT_SIZE);
+	glPointSize(x);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Initialize an instance", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) return 1;
+	glfwSwapInterval(1);
+	init(window);
+	while (!glfwWindowShouldClose(window)) {
+		display(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
