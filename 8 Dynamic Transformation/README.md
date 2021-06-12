42b02cf33903e00148684aef4671b5a8b7b387fc
Sachin Godishela <sachingodishela@outlook.com>
2021-06-12 16:18:13 +0530

    Using transformation matrix to animate

---
+++ 8 Dynamic Transformation/README.md
@@ -0,0 +1,3 @@
+# Animating The Triangle
+
+In this project, we animate the triangle that we've drawn previously.
---
+++ 8 Dynamic Transformation/vertShader.glsl
@@ -0,0 +1,38 @@
+#version 430
+uniform float offset;
+uniform float angle;
+
+mat4 buildRotateZ(float rad) {
+	mat4 zrot = mat4(cos(rad), -sin(rad), 0.0, 0.0,
+		sin(rad), cos(rad), 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0 );
+	return zrot;
+}
+
+mat4 buildTranslate(float x, float y, float z) {
+	mat4 trans = mat4(1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		x, y, z, 1.0);
+	return trans;
+}
+
+void main(void)
+{
+	mat4 rotMat = buildRotateZ(angle);
+	mat4 transMat = buildTranslate(offset, 0, 0);
+	mat4 transformMat =  transMat * rotMat;
+	if(gl_VertexID == 0) {
+		gl_Position = vec4(-0.25, 0, 0, 1);
+		gl_Position =  transformMat * gl_Position;
+	}
+	else if (gl_VertexID == 1) {
+		gl_Position = vec4(0.25, 0, 0, 1);
+		gl_Position = transformMat * gl_Position;
+	}
+	else {
+		gl_Position = vec4(0,0.7,0,1);
+		gl_Position = transformMat * gl_Position;
+	}
+}
